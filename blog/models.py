766f28d86d8c7e76151b6737278a2faa0b352d04
maheechy77 <maheechy96@gmail.com>
2020-01-10 11:10:29 +0600

    ADDed Blog

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,12 @@ class Blog(models.Model):
     pubdate=models.DateTimeField()
     body=models.TextField()
     image=models.ImageField(upload_to='blogimages/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:75]
+
+    def short_date(self):
+        return self.pubdate.strftime(' %b %e %Y')    
